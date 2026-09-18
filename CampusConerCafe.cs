8a007b13166c16de70f5e1151f35c4d4db2c7802
dreyers27 <s225407032@deakin.edu.au>
2026-09-18 12:58:56 +1000

    Initial CampusCornerCafe

---
+++ CampusConerCafe.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=== Campus Corner Cafe ===");
+        Console.WriteLine("Coffee: $4.50");
+        Console.WriteLine("Muffin: $3.00");
+
+        double total = 4.5 + 3.0;
+        Console.WriteLine($"Total: ${total}");
+    }
+}
